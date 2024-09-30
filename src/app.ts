0a02f722f57d5fe07ea68a86a7f0cd70c8ccfa47
Catherine Gaughan-Smith <catgsmith@users.noreply.github.com>
2024-09-30 21:01:44 +0100

    01_Arrow Functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('Ahoy TypeScript now!');
+const pizzas = [{ name: "Pepperoni", toppings: ["pepperoni"] }];
+// map return name in uppercaase
+const mappedPizzas = pizzas.map(
+  (pizza, i) => `${i}. ${pizza.name.toUpperCase()}`
+);
+
+console.log(mappedPizzas); // ['PEPPERONI']
+
+/* const pizza = {
+  name: "Blazing Inferno",
+  getName: function () {
+    setTimeout(() => {
+      console.log(this.name);
+    }, 100);
+  },
+}; */
+const pizza = {
+  name: "Blazing Inferno",
+  getName: () => pizza.name,
+};
+
+console.log(pizza.getName());
